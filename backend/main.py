d284185762798fb56183849cc13c744cc71bb1eb
Watcher <264154009+MonojitVE@users.noreply.github.com>
2026-06-08 19:49:51 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
 CORSMiddleware,
-allow_origins=["*"], # For demo purposes
+allow_origins=["https://dabar-demo.vercel.app/"], # For demo purposes
 allow_credentials=True,
 allow_methods=["*"],
 allow_headers=["*"],
